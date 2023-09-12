db0a373a67a9f554c76c57f138c49fe650519c39
= <=pdat9776@gmail.com>
2023-09-12 23:40:16 +0700

    Đạt sửa header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -3,7 +3,7 @@ import LayoutAdmin from "./SideBar/layout_admin"
 export default function Header() {
   return (
     <header>
-      <h1 className="text-white text-2xl font-bold">Header</h1>
+      {/* <h1 className="text-white text-2xl font-bold">Header</h1> */}
 
       <LayoutAdmin></LayoutAdmin>
     </header>
