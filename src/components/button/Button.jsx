4bb4fba168d62a1fe4e9d84ca5141b95b9c17753
VuHuyDucc <vuhuyduc2100@gmail.com>
2023-09-21 14:37:14 +0700

    huyduc fix tablefivecols, fix card, fix responsive card trang chu

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,26 +1,13 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  // Add FontAwesome icons for hiding and showing the menu
-  faBars,
-} from '@fortawesome/free-solid-svg-icons';
-// import './topbar.scss';
 const Button = (props) => {
-  const { text, style, Class, Icon, id } = props
+  const { text, style, Class, Icon, id } = props;
   return (
     <div>
-      <button style={style} id={""+id} className={" "+Class}>
+      <button style={style} id={"" + id} className={" " + Class}>
         {Icon && <Icon></Icon>}
         {text}
       </button>
     </div>
   );
-}
+};
 
 export default Button;
-
-
-
-
-
-
-
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,26 +1,28 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   // Add FontAwesome icons for hiding and showing the menu
   faBars,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 const Card = (props) => {
-  const { Icon, title, content,  Class } = props
+  const { Icon, title, content, unit, percent } = props;
   return (
-    <div className="mr-6 border-2 w-full p-4 rounded-lg bg-white">
-        {Icon && <Icon></Icon>}
-        {title}
-        {content}
-
+    <div className="w-full p-6 bg-white border-2 rounded-lg">
+      <div className="flex items-start justify-between">
+        <div className="">{Icon && <Icon></Icon>}</div>
+        <div className="px-2 text-lg font-medium bg-gray-200 rounded-lg text-emerald-600">
+          {percent}%
+        </div>
+      </div>
+      <div className="mt-6">
+        <div className="mb-2 text-base font-medium text-gray-500">{title}</div>
+        <div className="flex items-center text-2xl font-medium whitespace-wrap">
+          <div className="mr-2">
+            {content} {unit}
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
-}
+};
 
 export default Card;
-
-
-
-
-
-
-
