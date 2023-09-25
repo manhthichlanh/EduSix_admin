1d2d9b7ca148abd310d6ae3e8566372296eb8f77
VuHuyDucc <vuhuyduc2100@gmail.com>
2023-09-25 17:41:01 +0700

    huyduc fix responsive trang memberdetail

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,30 +1,27 @@
-import {Outlet} from "react-router-dom"
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 // import Footer from "./Footer";
 import TopBar from "./TopBar/topBar";
-import "./Layout.css";
-export default function Layout () {
-    return (
-        <div className="Layout_Admin-container">
-            <div className="Admin-container">
-                <div className="Header_Admin">
-                <Header></Header>
-                </div>
-                <div className="Site_Admin_Right">
-                <div className="TopBar_Admin">
-                <TopBar></TopBar>
-                </div>
-                <div className="Noidungchinh_Admin">
-                <Outlet></Outlet>
-                </div>
-                {/* <div className="Footer_Admin">
+import "./Layout.scss";
+export default function Layout() {
+  return (
+    <div className="Layout_Admin-container">
+      <div className="Admin-container">
+        <div className="Header_Admin">
+          <Header></Header>
+        </div>
+        <div className="Site_Admin_Right">
+          <div className="TopBar_Admin">
+            <TopBar></TopBar>
+          </div>
+          <div className="Noidungchinh_Admin">
+            <Outlet></Outlet>
+          </div>
+          {/* <div className="Footer_Admin">
                 <Footer></Footer>
                 </div> */}
-                </div>
-            </div>
-
         </div>
-
-
-    );
+      </div>
+    </div>
+  );
 }
